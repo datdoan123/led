f66cbf7c32554b21f4bc0640b7c0f87be7f46c50
datdoan123 <dat.dt04@gmail.com>
2019-09-19 19:34:33 +0700

    led

---
+++ led.ino
@@ -0,0 +1,14 @@
+
+#define led 12
+void setup() {pinMode(led,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {digitalWrite(led,HIGH);
+delay(20);
+digitalWrite(led,LOW);
+delay(20);
+  // put your main code here, to run repeatedly:
+
+}
